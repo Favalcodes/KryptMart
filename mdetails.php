709a645ab60486fd863237275803657c792f2907
Favour Godwin Arua <favourvalg@gmail.com>
2020-12-19 11:53:46 +0100

    separate files for php code

--- mdetails.php
+++ mdetails.php
@@ -1,301 +1,35 @@
 <?php
+if(isset($_POST['submit']))
+{
+	$category=$_POST['category'];
+	$productname=$_POST['p_name'];
+	$productamount=$_POST['amount'];
+	$discount=$_POST['discount'];
+    $moq=$_POST['moq'];
+	$download=$_POST['download'];
+	$productdescription=$_POST['description'];
+	$tags=$_POST['tags'];
+	$prod=$_FILES["product"]["name"];
+	// $product=$_POST['product'];
+	$productimage1=$_FILES["image_1"]["name"];
+	$productimage2=$_FILES["image_2"]["name"];
+	$productimage3=$_FILES["image_3"]["name"];
+	$productimage4=$_FILES["image_4"]["name"];
+//for getting product id
+$query=mysqli_query($link,"select max(id) as pid from manufacture");
+	$result=mysqli_fetch_array($query);
+	 $productid=$result['pid']+1;
+	$dir="productimages/$productid";
+	$path="manuproduct/$productid";
+	mkdir($dir);// directory creation for product images
+	mkdir($path);// directory creation for product
+	move_uploaded_file($_FILES["image_1"]["tmp_name"],"productimages/$productid/".$_FILES["image_1"]["name"]);
+	move_uploaded_file($_FILES["image_2"]["tmp_name"],"productimages/$productid/".$_FILES["image_2"]["name"]);
+	move_uploaded_file($_FILES["image_3"]["tmp_name"],"productimages/$productid/".$_FILES["image_3"]["name"]);
+	move_uploaded_file($_FILES["image_4"]["tmp_name"],"productimages/$productid/".$_FILES["image_4"]["name"]);
+	move_uploaded_file($_FILES["product"]["tmp_name"],"manuproduct/$productid/".$_FILES["product"]["name"]);
+$sql=mysqli_query($link,"insert into manufacture (user_id,category,amount,discount,moq,description,tags,p_name,image_1,image_2,image_3,image_4,download, product) values('$_SESSION[id]','$category','$productamount','$discount','$moq','$productdescription','$tags','$productname','$productimage1','$productimage2','$productimage3', '$productimage4', '$download', '$prod')");
+echo "<script>alert('Product Inserted Successfully!')</script>";
 
-// Define variables and initialize with empty values
-$p_name = $category = $amount = $discount = $tags = $moq = $description = $image_1 = $image_2 = $image_3 = $image_4 = $product = $download = "";
-$p_name_err = $category_err = $amount_err = $discount_err = $tags_err = $moq_err = $description_err = $image_1_err = $image_2_err = $image_3_err = $image_4_err = $product_err = $download_err = "";
-
-// Processing form data when form is submitted
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-
-  // Validate Product Name
-  if (empty(trim($_POST["p_name"]))) {
-    $p_name_err = "Please enter the Product Name.";
-  } else {
-    // Prepare a select statement
-    $sql = "SELECT id FROM products WHERE p_name = ?";
-
-    if ($stmt = mysqli_prepare($link, $sql)) {
-      // Bind variables to the prepared statement as parameters
-      mysqli_stmt_bind_param($stmt, "s", $param_p_name);
-
-      // Set parameters
-      $param_p_name = trim($_POST["p_name"]);
-
-      // Attempt to execute the prepared statement
-      if (mysqli_stmt_execute($stmt)) {
-        /* store result */
-        mysqli_stmt_store_result($stmt);
-        $p_name = trim($_POST["p_name"]);
-      } else {
-        echo "Oops! Something went wrong. Please try again later.";
-      }
-
-      // Close statement
-      mysqli_stmt_close($stmt);
-    }
-  }
-
-  // Validate category
-  if (empty(trim($_POST["category"]))) {
-    $category_err = "Please enter Category.";
-  } else {
-    // Prepare a select statement
-    $sql = "SELECT id FROM products WHERE category = ?";
-
-    if ($stmt = mysqli_prepare($link, $sql)) {
-      // Bind variables to the prepared statement as parameters
-      mysqli_stmt_bind_param($stmt, "s", $param_category);
-
-      // Set parameters
-      $param_category = trim($_POST["category"]);
-
-      // Attempt to execute the prepared statement
-      if (mysqli_stmt_execute($stmt)) {
-        /* store result */
-        mysqli_stmt_store_result($stmt);
-        $category = trim($_POST["category"]);
-      } else {
-        echo "Oops! Something went wrong. Please try again later.";
-      }
-
-      // Close statement
-      mysqli_stmt_close($stmt);
-    }
-  }
-
-  // Validate amount
-  if (empty(trim($_POST["amount"]))) {
-    $amount_err = "Please enter a valid amount.";
-  } else {
-    // Prepare a select statement
-    $sql = "SELECT id FROM products WHERE amount = ?";
-
-    if ($stmt = mysqli_prepare($link, $sql)) {
-      // Bind variables to the prepared statement as parameters
-      mysqli_stmt_bind_param($stmt, "s", $param_amount);
-
-      // Set parameters
-      $param_amount = trim($_POST["amount"]);
-
-      // Attempt to execute the prepared statement
-      if (mysqli_stmt_execute($stmt)) {
-        /* store result */
-        mysqli_stmt_store_result($stmt);
-          $amount = trim($_POST["amount"]);
-      } else {
-        echo "Oops! Something went wrong. Please try again later.";
-      }
-
-      // Close statement
-      mysqli_stmt_close($stmt);
-    }
-  }
-
-  // Validate tags
-  if (empty(trim($_POST["tags"]))) {
-    $tags_err = "Please enter Tags.";
-  } else {
-    // Prepare a select statement
-    $sql = "SELECT id FROM products WHERE tags = ?";
-
-    if ($stmt = mysqli_prepare($link, $sql)) {
-      // Bind variables to the prepared statement as parameters
-      mysqli_stmt_bind_param($stmt, "s", $param_tags);
-
-      // Set parameters
-      $param_tags = trim($_POST["tags"]);
-
-      // Attempt to execute the prepared statement
-      if (mysqli_stmt_execute($stmt)) {
-        /* store result */
-        mysqli_stmt_store_result($stmt);
-        $tags = trim($_POST["tags"]);
-      } else {
-        echo "Oops! Something went wrong. Please try again later.";
-      }
-
-      // Close statement
-      mysqli_stmt_close($stmt);
-    }
-  }
-
-  // Validate moq
-  if (empty(trim($_POST["moq"]))) {
-    $moq_err = "Please enter your moq.";
-  } else {
-    // Prepare a select statement
-    $sql = "SELECT id FROM products WHERE moq = ?";
-
-    if ($stmt = mysqli_prepare($link, $sql)) {
-      // Bind variables to the prepared statement as parameters
-      mysqli_stmt_bind_param($stmt, "s", $param_moq);
-
-      // Set parameters
-      $param_moq = trim($_POST["moq"]);
-
-      // Attempt to execute the prepared statement
-      if (mysqli_stmt_execute($stmt)) {
-        /* store result */
-        mysqli_stmt_store_result($stmt);
-        $moq = trim($_POST["moq"]);
-      } else {
-        echo "Oops! Something went wrong. Please try again later.";
-      }
-
-      // Close statement
-      mysqli_stmt_close($stmt);
-    }
-  }
-
-  // Validate description
-  if (empty(trim($_POST["description"]))) {
-    $description_err = "Please enter Description.";
-  } else {
-    // Prepare a select statement
-    $sql = "SELECT id FROM products WHERE description = ?";
-
-    if ($stmt = mysqli_prepare($link, $sql)) {
-      // Bind variables to the prepared statement as parameters
-      mysqli_stmt_bind_param($stmt, "s", $param_description);
-
-      // Set parameters
-      $param_description = trim($_POST["description"]);
-
-      // Attempt to execute the prepared statement
-      if (mysqli_stmt_execute($stmt)) {
-        /* store result */
-        mysqli_stmt_store_result($stmt);
-        $description = trim($_POST["description"]);
-      } else {
-        echo "Oops! Something went wrong. Please try again later.";
-      }
-
-      // Close statement
-      mysqli_stmt_close($stmt);
-    }
-  }
-
-//   // Validate image_1
-//   if (empty(trim($_POST["image_1"]))) {
-//     $image_1_err = "Please Upload Image.";
-//   } else {
-//     // Prepare a select statement
-//     $sql = "SELECT id FROM products WHERE image_1 = ?";
-
-//     if ($stmt = mysqli_prepare($link, $sql)) {
-//       // Bind variables to the prepared statement as parameters
-//       mysqli_stmt_bind_param($stmt, "s", $param_image_1);
-
-//       // Set parameters
-//       $param_image_1 = trim($_POST["image_1"]);
-
-//       // Attempt to execute the prepared statement
-//       if (mysqli_stmt_execute($stmt)) {
-//         /* store result */
-//         mysqli_stmt_store_result($stmt);
-//         $image_1 = trim($_POST["image_1"]);
-//       } else {
-//         echo "Oops! Something went wrong. Please try again later.";
-//       }
-
-//       // Close statement
-//       mysqli_stmt_close($stmt);
-//     }
-//   }
-  
-  // Validate Product
-//   if (empty(trim($_POST["product"]))) {
-//     $product_err = "Please Upload Product.";
-//   } else {
-//     // Prepare a select statement
-//     $sql = "SELECT id FROM products WHERE product = ?";
-
-//     if ($stmt = mysqli_prepare($link, $sql)) {
-//       // Bind variables to the prepared statement as parameters
-//       mysqli_stmt_bind_param($stmt, "s", $param_product);
-
-//       // Set parameters
-//       $param_product = trim($_POST["product"]);
-
-//       // Attempt to execute the prepared statement
-//       if (mysqli_stmt_execute($stmt)) {
-//         /* store result */
-//         mysqli_stmt_store_result($stmt);
-//         $product = trim($_POST["product"]);
-//       } else {
-//         echo "Oops! Something went wrong. Please try again later.";
-//       }
-
-//       // Close statement
-//       mysqli_stmt_close($stmt);
-//     }
-//   }
-
-//   // Validate Product Type
-//   if (empty(trim($_POST["download"]))) {
-//     $download_err = "Please enter Number of downloads.";
-//   } else {
-//     // Prepare a select statement
-//     $sql = "SELECT id FROM products WHERE download = ?";
-
-//     if ($stmt = mysqli_prepare($link, $sql)) {
-//       // Bind variables to the prepared statement as parameters
-//       mysqli_stmt_bind_param($stmt, "s", $param_download);
-
-//       // Set parameters
-//       $param_download = trim($_POST["download"]);
-
-//       // Attempt to execute the prepared statement
-//       if (mysqli_stmt_execute($stmt)) {
-//         /* store result */
-//         mysqli_stmt_store_result($stmt);
-//         $download = trim($_POST["download"]);
-//       } else {
-//         echo "Oops! Something went wrong. Please try again later.";
-//       }
-
-//       // Close statement
-//       mysqli_stmt_close($stmt);
-//     }
-//   }
-
-  // Check input errors before inserting in database
-  if (empty($p_name_err) && empty($category_err) && empty($amount_err) && empty($image_1_err) && empty($tags_err) && empty($moq_err) && empty($description_err) && empty($download_err)) {
-
-    // Prepare an insert statement
-    $sql = "INSERT INTO products (p_name, category, amount, discount, tags, moq, description, image_1, image_2, image_3, image_4, product, download) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
-
-    if ($stmt = mysqli_prepare($link, $sql)) {
-      // Bind variables to the prepared statement as parameters
-      mysqli_stmt_bind_param($stmt, "sssssssssssss", $param_p_name, $param_category, $param_amount, $param_discount, $param_tags, $param_moq, $param_description, $param_image_1, $param_image_2, $param_image_3, $param_image_4, $param_product, $param_download);
-
-      // Set parameters
-      $param_p_name = $p_name;
-      $param_category = $category;
-      $param_amount = $amount;
-      $param_discount = $discount;
-      $param_tags = $tags;
-      $param_moq = $moq;
-      $param_description = $description;
-      $param_download = $download;
-      $param_product = $product;
-
-      // Attempt to execute the prepared statement
-      if (mysqli_stmt_execute($stmt)) {
-
-        echo "<script>window.alert('Product Uploaded Successfully')</script>";
-      } else {
-        echo "Something went wrong. Please try again later.";
-      }
-
-      // Close statement
-      mysqli_stmt_close($stmt);
-    }
-  }
-
-
-
-  // Close connection
-  mysqli_close($link);
 }
 ?>
--- vendor.php
+++ vendor.php
@@ -10,38 +10,8 @@ if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true) {
 // include config file
 include 'config.php';
 
-if(isset($_POST['submit']))
-{
-	$category=$_POST['category'];
-	$productname=$_POST['p_name'];
-	$productamount=$_POST['amount'];
-	$discount=$_POST['discount'];
-	$download=$_POST['download'];
-	$productdescription=$_POST['description'];
-	$tags=$_POST['tags'];
-	$prod=$_FILES["product"]["name"];
-	// $product=$_POST['product'];
-	$productimage1=$_FILES["image_1"]["name"];
-	$productimage2=$_FILES["image_2"]["name"];
-	$productimage3=$_FILES["image_3"]["name"];
-	$productimage4=$_FILES["image_4"]["name"];
-//for getting product id
-$query=mysqli_query($link,"select max(id) as pid from vendor");
-	$result=mysqli_fetch_array($query);
-	 $productid=$result['pid']+1;
-	$dir="sellerimages/$productid";
-	$path="vendorproduct/$productid";
-	mkdir($dir);// directory creation for product images
-	mkdir($path);// directory creation for product
-	move_uploaded_file($_FILES["image_1"]["tmp_name"],"sellerimages/$productid/".$_FILES["image_1"]["name"]);
-	move_uploaded_file($_FILES["image_2"]["tmp_name"],"sellerimages/$productid/".$_FILES["image_2"]["name"]);
-	move_uploaded_file($_FILES["image_3"]["tmp_name"],"sellerimages/$productid/".$_FILES["image_3"]["name"]);
-	move_uploaded_file($_FILES["image_4"]["tmp_name"],"sellerimages/$productid/".$_FILES["image_4"]["name"]);
-	move_uploaded_file($_FILES["product"]["tmp_name"],"vendorproduct/$productid/".$_FILES["product"]["name"]);
-$sql=mysqli_query($link,"insert into vendor(user_id,category,amount,discount,description,tags,p_name,image_1,image_2,image_3,image_4,download, product) values('$_SESSION[id]','$category','$productamount','$discount','$productdescription','$tags','$productname','$productimage1','$productimage2','$productimage3', '$productimage4', '$download', '$prod')");
-echo "<script>alert('Product Inserted Successfully!')</script>";
-
-}
+// include vendor php details
+include 'vdetails.php';
 
 // SQL query to select data from database 
 $sql = "SELECT * FROM users where id = $_SESSION[id]";
